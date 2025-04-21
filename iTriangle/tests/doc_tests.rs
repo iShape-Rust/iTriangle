28bcf2a5cfe11fd62c8fa8fbdd51fd7c7607e415
Nail Sharipov <nailxsharipov@gmail.com>
2025-04-21 22:15:33 +0300

    update doc test and image

--- iTriangle/tests/doc_tests.rs
+++ iTriangle/tests/doc_tests.rs
@@ -1,42 +1,43 @@
 #[cfg(test)]
 mod tests {
-    use i_overlay::i_float::float::point::FloatPoint;
     use i_triangle::float::triangulatable::Triangulatable;
 
     #[test]
     fn test_0() {
-        let shape = [
-            [ // body
-                FloatPoint::new(0.0, 20.0),    // 0
-                FloatPoint::new(8.0, 10.0),    // 1
-                FloatPoint::new(7.0, 6.0),     // 2
-                FloatPoint::new(9.0, 1.0),     // 3
-                FloatPoint::new(13.0, -1.0),   // 4
-                FloatPoint::new(17.0, 1.0),    // 5
-                FloatPoint::new(26.0, -7.0),   // 6
-                FloatPoint::new(14.0, -15.0),  // 7
-                FloatPoint::new(0.0, -18.0),   // 8
-                FloatPoint::new(-14.0, -15.0), // 9
-                FloatPoint::new(-25.0, -7.0),  // 10
-                FloatPoint::new(-18.0, 0.0),   // 11
-                FloatPoint::new(-16.0, -3.0),  // 12
-                FloatPoint::new(-13.0, -4.0),  // 13
-                FloatPoint::new(-8.0, -2.0),   // 14
-                FloatPoint::new(-6.0, 2.0),    // 15
-                FloatPoint::new(-7.0, 6.0),    // 16
-                FloatPoint::new(-10.0, 8.0)    // 17
-            ].to_vec(),
-            [ // hole
-                FloatPoint::new(2.0, 0.0),     // 18
-                FloatPoint::new(-2.0, -2.0),   // 19
-                FloatPoint::new(-4.0, -5.0),   // 20
-                FloatPoint::new(-2.0, -9.0),   // 21
-                FloatPoint::new(2.0, -11.0),   // 22
-                FloatPoint::new(5.0, -9.0),    // 23
-                FloatPoint::new(7.0, -5.0),    // 24
-                FloatPoint::new(5.0, -2.0)     // 25
-            ].to_vec()
-        ].to_vec();
+        let shape = vec![
+            vec![
+                // body
+                [0.0, 20.0],    // 0
+                [8.0, 10.0],    // 1
+                [7.0, 6.0],     // 2
+                [9.0, 1.0],     // 3
+                [13.0, -1.0],   // 4
+                [17.0, 1.0],    // 5
+                [26.0, -7.0],   // 6
+                [14.0, -15.0],  // 7
+                [0.0, -18.0],   // 8
+                [-14.0, -15.0], // 9
+                [-25.0, -7.0],  // 10
+                [-18.0, 0.0],   // 11
+                [-16.0, -3.0],  // 12
+                [-13.0, -4.0],  // 13
+                [-8.0, -2.0],   // 14
+                [-6.0, 2.0],    // 15
+                [-7.0, 6.0],    // 16
+                [-10.0, 8.0],   // 17
+            ],
+            vec![
+                // hole
+                [2.0, 0.0],   // 18
+                [-2.0, -2.0], // 19
+                [-4.0, -5.0], // 20
+                [-2.0, -9.0], // 21
+                [2.0, -11.0], // 22
+                [5.0, -9.0],  // 23
+                [7.0, -5.0],  // 24
+                [5.0, -2.0],  // 25
+            ],
+        ];
 
         let triangulation = shape.triangulate().to_triangulation();
 
