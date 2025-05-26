f8420cbc6534407544460b769351bd321ce9323e
Nail Sharipov <nailxsharipov@gmail.com>
2025-05-26 17:06:06 +0300

    pub geom

--- iTriangle/src/lib.rs
+++ iTriangle/src/lib.rs
@@ -3,9 +3,9 @@ extern crate alloc;
 
 pub mod advanced;
 pub mod float;
-pub mod tessellation;
-mod geom;
-mod index;
+pub mod geom;
 pub mod int;
+pub mod tessellation;
+mod index;
 
 pub use i_overlay;
